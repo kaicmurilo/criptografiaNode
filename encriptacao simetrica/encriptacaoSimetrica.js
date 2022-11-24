38eaf9f1aabb1043de7193a52d7e6b92efc58e24
kaicmurilo <kaicmurilo@live.com>
2022-11-24 14:55:40 -0400

    encriptacao simetrica/

---
+++ encriptacao simetrica/encriptacaoSimetrica.js
@@ -0,0 +1,25 @@
+import { createCipheriv, randomBytes, createDecipheriv } from 'crypto'
+
+const mensagem = ' Testando essa cripto'
+const chave = randomBytes(32)
+const vi = randomBytes(16)
+
+const cifra = createCipheriv('aes256', chave, vi)
+
+const mensagemCifrada = cifra.update(mensagem, 'utf-8', 'hex') + cifra.final('hex')
+
+
+console.log(mensagemCifrada)
+
+
+//transmissao ----- chave, vi, mensagem 
+
+
+//decifrar mensagem
+
+const decifra = createDecipheriv('aes256', chave, vi)
+
+const mensagemDecifrada = decifra.update(mensagemCifrada, 'hex', 'utf-8') + decifra.final('utf-8')
+
+console.log(`Decifrada: ${mensagemDecifrada.toString('utf-8')}`)
+
